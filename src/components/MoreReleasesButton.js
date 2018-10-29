d33c105222197cd859d519a6cdaf6d42d37584ac
Michael Blumenthal <bloomer25@googlemail.com>
2018-10-29 15:43:02 +0000

    sets up the import export for all files

---
+++ src/components/MoreReleasesButton.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+class MoreReleasesButton extends Component{
+
+}
+
+export default MoreReleasesButton;
---
+++ src/components/ReleaseTitle.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+class ReleaseTitle extends Component{
+
+}
+
+export default ReleaseTitle;
