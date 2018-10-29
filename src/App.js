9e3602158a982ee977a3dad10c5d6763ae402128
Michael Blumenthal <bloomer25@googlemail.com>
2018-10-29 15:40:50 +0000

    gets releasesbox container working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReleasesBox from './containers/ReleasesBox.js';
 
 class App extends Component {
+
   render() {
     return (
-
+      <ReleasesBox/>
     );
   }
 }
